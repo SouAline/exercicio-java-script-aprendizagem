2732cd1a87bf3b4e1f1308cd56cd96f5f5ca48cc
Aline Souza <ds.alinesouza@gmail.com>
2022-01-10 13:17:09 -0300

    criando componentes reutilizaveis

--- components/src/index.js
+++ components/src/index.js
@@ -6,9 +6,21 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetails author="Alex" timeAgo="16:45" content="Nice blog post" />
-      <CommentDetails author="Jane" timeAgo="10:00" content="Nice blog" />
-      <CommentDetails author="Joane" timeAgo="02:00" content="Nice photo" />
+      <ApprovalCard>
+        <CommentDetails
+          author="Alex"
+          timeAgo="16:45"
+          content="Nice blog post"
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetails author="Jane" timeAgo="10:00" content="Nice blog" />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetails author="Joane" timeAgo="02:00" content="Nice photo" />
+      </ApprovalCard>
     </div>
   );
 };
