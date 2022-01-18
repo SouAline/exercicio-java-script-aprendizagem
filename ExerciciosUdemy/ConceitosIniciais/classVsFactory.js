0f1894d59daad600b6d4b9f840b8ac0ec5732723
Aline Souza <ds.alinesouza@gmail.com>
2022-01-17 21:48:56 -0300

    aprendendo a diferenca entre classe e factory

---
+++ ExerciciosUdemy/ConceitosIniciais/classVsFactory.js
@@ -0,0 +1,12 @@
+class Pessoa {
+  constructor(nome) {
+    this.nome = nome;
+  }
+
+  falar() {
+    console.log(`Meu nome é ${this.nome}`);
+  }
+}
+
+const pessoa1 = new Pessoa("Joao");
+pessoa1.falar();
