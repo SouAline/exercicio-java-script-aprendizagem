af451e0f2444c828c354bfce1bb3bc54e38e13a4
Aline Souza <ds.alinesouza@gmail.com>
2022-01-25 21:20:33 -0300

    aprende funcoes importante de objetos

---
+++ ExerciciosUdemy/Objeto/funcoesImportantes.js
@@ -0,0 +1,9 @@
+const pessoa = {
+  nome: "Rebeca",
+  idade: 2,
+  peso: 13,
+};
+
+console.log(Object.keys(pessoa)); // tras os atibutos
+console.log(Object.values(pessoa)); // tras os valores
+console.log(Object.entries(pessoa)); // tras um array de pessoa com chave e valor separado por virgula
