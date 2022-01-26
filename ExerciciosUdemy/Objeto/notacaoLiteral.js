a485c8adaa81599a580d8f5d8675920953a98315
Aline Souza <ds.alinesouza@gmail.com>
2022-01-25 21:19:57 -0300

    aprende conceito notacao literal

--- ExerciciosUdemy/Objeto/notacaoLiteral.js
+++ ExerciciosUdemy/Objeto/notacaoLiteral.js
@@ -9,3 +9,20 @@ console.log(objeto1, objeto2);
 
 const nomeAtributo = "nota";
 const valorAtributo = 7.8;
+
+const objeto3 = {};
+objeto3[nomeAtributo] = valorAtributo;
+console.log(objeto3);
+
+const objeto4 = { [nomeAtributo]: valorAtributo };
+console.log(objeto4);
+
+const objeto5 = {
+  funcao: function () {
+    //...
+  },
+  funcao2() {
+    //..
+  }, //As duas formas dessa funcao funcionam
+};
+console.log(objeto5);
