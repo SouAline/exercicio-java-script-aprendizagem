992be4e64c5e5a359bdf5bf6ca9cecf24c8d31a7
Aline Souza <ds.alinesouza@gmail.com>
2022-01-25 21:21:13 -0300

    aprende getter e setter

---
+++ ExerciciosUdemy/Objeto/getterSetter.js
@@ -0,0 +1,16 @@
+const sequencia = {
+  _valor: 1, //underline é usado por convencao para indicar que esta variavel é pra ser usada dentro do obj
+  get valor() {
+    return this._valor++;
+  },
+  set valor(valor) {
+    if (valor > this._valor) {
+      this._valor = valor;
+    }
+  },
+};
+console.log(sequencia.valor, sequencia.valor);
+sequencia.valor = 1000;
+console.log(sequencia.valor, sequencia.valor);
+sequencia.valor = 900;
+console.log(sequencia.valor, sequencia.valor);
