ef55f2d3da0ad86ac9a16bb0eaf209668aecad3b
Aline Souza <ds.alinesouza@gmail.com>
2022-01-26 21:41:40 -0300

    continua exercicios de funcoes

--- ExerciciosUdemy/Objeto/funcoesImportantes.js
+++ ExerciciosUdemy/Objeto/funcoesImportantes.js
@@ -7,3 +7,15 @@ const pessoa = {
 console.log(Object.keys(pessoa)); // tras os atibutos
 console.log(Object.values(pessoa)); // tras os valores
 console.log(Object.entries(pessoa)); // tras um array de pessoa com chave e valor separado por virgula
+
+Object.entries(pessoa).forEach(([chave, valor]) => {
+  // vai em objeto pessoa, receve a chave e o valor e imprime como solicitado
+  console.log(`${chave}: ${valor}`);
+});
+
+Object.defineProperty(pessoa, "dataNascimento", {
+  enumerable: true,
+  writable: false,
+  value: "10/12/2022",
+});
+console.log(pessoa);
